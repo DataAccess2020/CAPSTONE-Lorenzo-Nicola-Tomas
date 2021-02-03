edb6cb75774c7769a9bc00da4b7794c1816c110c
LORENZO SIOLI <lorenzo.sioli98@gmail.com>
2021-02-03 17:31:12 +0100

    data mgmt update

--- scripts/02_Data_MNGMNT.R
+++ scripts/02_Data_MNGMNT.R
@@ -8,3 +8,6 @@ end_gov<-tibble::rowid_to_column(end_gov, "ID")
 
 ita_governments<- merge(ita_govs,end_gov,by="ID") 
 
+#removing useless variables
+
+govs_ita= subset(ita_governments, select = -c(country_name_short, country_name, party_name_english,country_id, election_id, cabinet_id, previous_cabinet_id, party_id,left_right, party_name_short, cabinet_party,prime_minister))
