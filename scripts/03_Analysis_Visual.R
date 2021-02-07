ea4f2d9c3616eea252a514fbfb0b9391cd3da44c
Haoics <tomas.ruzza.12@gmail.com>
2021-02-07 12:29:10 +0100

    pie chart

--- scripts/03_Analysis_Visual.R
+++ scripts/03_Analysis_Visual.R
@@ -6,14 +6,14 @@ summary(italy$crisis_days)
 
 
 
-count(italy, outcomes)
-
-ggpie<- data.frame(group= c("Constitutional Referendum", "Early Election", "End Legislature", "Majority Changed", "Majority Confirmed", "Technocratic Gov."), value=c(1, 11, 6, 21, 23, 3))
-
+conta <- count(italy, outcomes) 
+conta
 
 dev.off()
 # Basic piechart
-ggplot(ggpie, aes(x="", y=value, fill=group)) +
+ggplot(conta, aes(x="", y=n, fill=outcomes)) +
   geom_bar(stat="identity", color="white") +
   coord_polar("y", start=0) +
   theme_void() # remove background, grid, numeric labels
+
+
