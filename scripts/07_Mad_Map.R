050b51722a52df3fabd491f2d79f2216a1adc7d7
Haoics <tomas.ruzza.12@gmail.com>
2021-02-12 17:56:34 +0100

    ping pong map

--- scripts/07_Mad_Map.R
+++ scripts/07_Mad_Map.R
@@ -1,4 +1,8 @@
-
+library("ggmap")
+library(mapdata)
+library(maptools)
+library(rnaturalearth)
+library(rnaturalearthdata)
 
 capmap<-read.csv(here::here ("dataset", "concap.csv"))
 capmap<-dplyr::filter(capmap, capmap$ContinentName == "Europe")
@@ -6,13 +10,13 @@ capmap<- dplyr::rename(capmap, state = CountryName)
 
 techmap<-merge(capmap, techn, by = "state")
 
-#Load the library
-install.packages("mapdata")
-install.packages("maptools")
-library("ggmap")
-library(mapdata)
-library(maptools)
+#europe map-----
 
+world <- ne_countries(scale = "medium", returnclass = "sf")
+Europe <- world[which(world$continent == "Europe"),]
+ggplot(Europe) +
+  geom_sf() +
+  coord_sf(xlim = c(-25,50), ylim = c(35,70), expand = FALSE)
 
 
 
