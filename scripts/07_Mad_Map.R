21f3de84c8472f2a6ceb9d74c779d4fcaa16f7dc
Nicola Destro <nicola.destro.cap@gmail.com>
2021-02-12 18:35:14 +0100

    map update

--- scripts/07_Mad_Map.R
+++ scripts/07_Mad_Map.R
@@ -1,8 +1,12 @@
 library("ggmap")
 library(mapdata)
 library(maptools)
+install.packages("rgeos")
+install.packages('rnaturalearth')
+install.packages('rnaturalearthdata')
 library(rnaturalearth)
 library(rnaturalearthdata)
+library(rgeos)
 
 capmap<-read.csv(here::here ("dataset", "concap.csv"))
 capmap<-dplyr::filter(capmap, capmap$ContinentName == "Europe")
@@ -14,9 +18,21 @@ techmap<-merge(capmap, techn, by = "state")
 
 world <- ne_countries(scale = "medium", returnclass = "sf")
 Europe <- world[which(world$continent == "Europe"),]
-ggplot(Europe) +
+EU<-ggplot(Europe) +
   geom_sf() +
   coord_sf(xlim = c(-25,50), ylim = c(35,70), expand = FALSE)
+
+
+Map1<-EU + geom_point(aes(x = CapitalLongitude, y = CapitalLatitude), data = techmap, size = 2.5) + 
+  theme(legend.position="bottom")
+ggplotly(Map1)
+
+
+
+
+
+rm(concap, europe, view_cabinet)
+
 
 
 
