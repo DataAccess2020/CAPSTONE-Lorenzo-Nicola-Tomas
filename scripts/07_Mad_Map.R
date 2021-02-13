f708c06be1c84ac5c7a59dff36ad7e376c8bed1b
Nicola Destro <nicola.destro.cap@gmail.com>
2021-02-13 01:37:33 +0100

    Map enhancement

--- scripts/07_Mad_Map.R
+++ scripts/07_Mad_Map.R
@@ -1,9 +1,9 @@
 library("ggmap")
 library(mapdata)
 library(maptools)
-install.packages("rgeos")
-install.packages('rnaturalearth')
-install.packages('rnaturalearthdata')
+#install.packages("rgeos")
+#install.packages('rnaturalearth')
+#install.packages('rnaturalearthdata')
 library(rnaturalearth)
 library(rnaturalearthdata)
 library(rgeos)
@@ -22,10 +22,20 @@ EU<-ggplot(Europe) +
   geom_sf() +
   coord_sf(xlim = c(-25,50), ylim = c(35,70), expand = FALSE)
 
+N<-c(3,3,3,2,2,3,3,3,5,5,5,5,5,1,3,3,3,2,2,5,5,5,5,5)
+N<-as.data.frame(N)
+N<-tibble::rowid_to_column(N, "ID")
+techmap<-tibble::rowid_to_column(techmap, "ID")
+techmap<-merge(N,techmap, by= "ID")
 
-Map1<-EU + geom_point(aes(x = CapitalLongitude, y = CapitalLatitude), data = techmap, size = 2.5) + 
+
+
+
+Map1<-EU + geom_point(aes(x = CapitalLongitude, y = CapitalLatitude, label=state, label2=N, label3=tot_days), data = techmap, size = 2.5) + 
   theme(legend.position="bottom")
 ggplotly(Map1)
+
+
 
 
 
