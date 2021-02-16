683e6b220e912e4b1f077567bf655de1a84ad950
Haoics <tomas.ruzza.12@gmail.com>
2021-02-16 16:48:11 +0100

    plot

--- scripts/08_Caretaker_Comp.R
+++ scripts/08_Caretaker_Comp.R
@@ -1,7 +1,7 @@
 #TECH COMPARISON
 
+#caretakers reasons----
 
- 
 trigger_for_technocracy<- c("post-communism transition","post-communism transition", "global debt crisis","global debt crisis", "post-communism transition", "internal reasons", "internal reasons", "internal reasons", "internal reasons", "global debt crisis", "global debt crisis", "internal reasons", "internal reasons", "global debt crisis", "internal reasons", "internal reasons", "global debt crisis", "internal reasons", "internal reasons", "post-communism transition","post-communism transition","post-communism transition","post-communism transition","post-communism transition")
 trigger_for_technocracy<-as.data.frame(trigger_for_technocracy)
 
@@ -11,5 +11,21 @@ trigger_for_technocracy<-tibble::rowid_to_column(trigger_for_technocracy, "ID")
 techn<-tibble::rowid_to_column(techn, "ID")
 techn_comp<-merge(trigger_for_technocracy, techn, by="ID")
 
+contaplot <- trigger_for_technocracy %>%
+  group_by(trigger_for_technocracy) %>%
+  summarise(count=n())
 
-ggplot(techn_comp, aes(x =techn_comp$trigger_for_technocracy)) + geom_bar(alpha = 0.2, col = "black") +  theme_bw()
+cols <- c("internal reasons"="gold1","post-communism transition"="firebrick2", "global debt crisis"="dodgerblue2")
+
+plottacont <- ggplot(contaplot, aes(x=reorder(trigger_for_technocracy,-count),y=count, fill = trigger_for_technocracy)) +  
+  geom_bar(stat = "identity") +
+  xlab(" ") +
+  ylab(" ") +
+  ggtitle("Caretaking triggers") +
+  scale_fill_manual(values = cols)
+
+plottacont
+
+ggplotly(plottacont)
+
+
