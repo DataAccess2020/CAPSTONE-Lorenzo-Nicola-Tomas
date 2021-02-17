dae4503b6ff6bba118d942b61352d0cbe272346f
Haoics <tomas.ruzza.12@gmail.com>
2021-02-17 12:15:25 +0100

    download articles

--- scripts/09_newspapers_scraping.R
+++ scripts/09_newspapers_scraping.R
@@ -4,13 +4,17 @@ library("rvest")
 library("stringr")
 
 ##guardian monti
-GM<-vector(mode= "list", length = 5)
+gm <- vector(mode= "list", length = 5)
 
-GM[1]<-"https://www.theguardian.com/business/blog/2012/may/30/mario-monti-italy-austerity"
-GM[2]<-"https://www.theguardian.com/business/2011/nov/29/euro-italy-insolvency-warning-finance-ministers"
-GM[3]<-"https://www.theguardian.com/world/2011/nov/13/mario-monti-new-era-italy"
-GM[4]<-"https://www.theguardian.com/world/2011/nov/11/markets-italy-applaud-mario-monti"
-GM[5]<-"https://www.theguardian.com/business/2011/nov/14/italy-borrowing-costs-highest-euro"
+gm[1]<-"https://www.theguardian.com/business/blog/2012/may/30/mario-monti-italy-austerity"
+gm[2]<-"https://www.theguardian.com/business/2011/nov/29/euro-italy-insolvency-warning-finance-ministers"
+gm[3]<-"https://www.theguardian.com/world/2011/nov/13/mario-monti-new-era-italy"
+gm[4]<-"https://www.theguardian.com/world/2011/nov/11/markets-italy-applaud-mario-monti"
+gm[5]<-"https://www.theguardian.com/business/2011/nov/14/italy-borrowing-costs-highest-euro"
+
+for (i in 1:5) {
+  download.file(url = gm[[i]], destfile = str_c("GM", i, ".html"))
+}
 
 ##GUARDIAN DRAGHI
 
@@ -27,7 +31,9 @@ GD[8]<-"https://www.theguardian.com/business/2021/feb/03/enter-the-draghi-can-su
 GD[9]<-"https://www.theguardian.com/commentisfree/2021/feb/04/the-guardian-view-on-mario-draghi-the-right-man-for-italy-for-now"
 GD[10]<-"https://www.theguardian.com/world/2021/feb/03/mario-draghi-accepts-mandate-to-form-new-italian-government"
 
-
+for (i in 1:10) {
+  download.file(url = GD[[i]], destfile = str_c("GD", i, ".html"))
+}
 ##LEMONDE MONTI
 
 LMM<-vector(mode = "list", length = 5)
@@ -38,6 +44,9 @@ LMM[3]<-"https://www.lemonde.fr/europe/article/2011/11/14/l-arrivee-de-mario-mon
 LMM[4]<-"https://www.lemonde.fr/idees/article/2011/11/16/et-si-l-italie-de-mario-monti-sauvait-l-europe_1604498_3232.html"
 LMM[5]<-"https://www.lemonde.fr/europe/article/2011/11/16/mario-monti-forme-son-gouvernement-et-prend-le-portefeuille-de-l-economie_1604643_3214.html"
 
+for (i in 1:5) {
+  download.file(url = LMM[[i]], destfile = str_c("LMM", i, ".html"))
+}
 
 ##LEMONDE DRAGHI
 
@@ -49,7 +58,9 @@ LMD[3]<-"https://www.lemonde.fr/international/article/2021/02/13/italie-mario-dr
 LMD[4]<-"https://www.lemonde.fr/international/article/2021/02/03/en-italie-sergio-mattarella-recourt-a-mario-draghi-pour-trouver-une-issue-a-la-crise-politique_6068625_3210.html"
 LMD[5]<-"https://www.lemonde.fr/idees/article/2019/03/04/mario-draghi-l-europe-la-souverainete-et-la-mondialisation_5431074_3232.html"
 
-
+for (i in 1:5) {
+  download.file(url = LMD[[i]], destfile = str_c("LMD", i, ".html"))
+}
 
 
 ##BBC MONTI
@@ -61,6 +72,9 @@ BBM[3] <- "http://www.bbc.co.uk/news/magazine-15720438"
 BBM[4] <- "https://www.bbc.com/news/magazine-15560015"
 BBM[5] <- "http://www.bbc.co.uk/news/world-europe-15650828"
 
+for (i in 1:5) {
+  download.file(url = BBM[[i]], destfile = str_c("BBM", i, ".html"))
+}
 ##BBC DRAGHI
 BBD <- vector(mode = "list", length = 4)
 
@@ -69,5 +83,8 @@ BBD[2] <-"https://www.bbc.com/news/world-europe-55992264"
 BBD[3] <-"https://www.bbc.com/news/world-europe-5591032"
 BBD[4] <-"https://www.bbc.com/news/world-europe-56049115"
 
+for (i in 1:5) {
+  download.file(url = BBD[[i]], destfile = str_c("BBD", i, ".html"))
+}
 
 
